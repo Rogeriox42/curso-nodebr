4584e2ae8c2acbb7471109bb2e90989baaf2b57e
Rogerio Rodrigues <sp.rogerio@outlook.com>
2019-12-24 14:38:31 -0300

    Sequelize installation and basic commands

--- 08-multi-db-postgres/src/scripts/postgres.sql
+++ 08-multi-db-postgres/src/scripts/postgres.sql
@@ -6,7 +6,19 @@ CREATE TABLE TB_HEROIS(
     PODER TEXT NOT NULL 
 ); 
 
+--create 
 INSERT INTO TB_HEROIS (NOME, PODER) VALUES
 ('Flash', 'Velocidade'), 
 ('Aquaman', 'Falar com Peixe'), 
 ('Batman', 'Dinheiro') 
+
+--read 
+SELECT * FROM TB_HEROIS 
+SELECT * FROM TB_HEROIS WHERE NOME = 'Batman'
+SELECT * FROM TB_HEROIS WHERE ID = '2'
+
+--update 
+UPDATE TB_HEROIS SET PODER = 'Nadar' WHERE NOME = 'Aquaman'
+UPDATE TB_HEROIS SET PODER = 'Alterar linha do tempo' WHERE NOME = 'Flash'
+
+--delete 
